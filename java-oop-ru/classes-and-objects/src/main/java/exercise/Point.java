6104966981d04cad8aacabbf806f5c6fda05fb03
@hexlet/cli <support@hexlet.io>
2023-11-26 22:09:49 +0300

    submit java-oop-ru/classes-and-objects

---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
@@ -0,0 +1,22 @@
+package exercise;
+
+// BEGIN
+class Point{
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    private  int x;
+    private  int y;
+
+    public Point(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+// END
